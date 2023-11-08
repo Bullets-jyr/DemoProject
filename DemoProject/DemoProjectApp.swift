e118025fd4e485847ace97886b4ba9ffbd881dd0
Bullets-jyr <jyr@bullets.co.kr>
2023-11-08 20:09:44 +0900

    Initial Commit

---
+++ DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by xinapse on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
